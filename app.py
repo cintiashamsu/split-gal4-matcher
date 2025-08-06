0d5fc8cf5770469a29405c36c75eefafc0a6cd62
Cintia <81837982+cintiashamsu@users.noreply.github.com>
2025-08-06 04:36:58 -0400

    Fly emoji

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ def analyze_match_image(match_img_path, output_dir):
     }
 
 # Streamlit UI
-st.title("Split-GAL4 Matcher")
+st.title("🪰 Split-GAL4 Matcher")
 
 ad = st.text_input("Enter AD Line")
 dbd = st.text_input("Enter DBD Line")
